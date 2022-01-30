5a3c1956fc3b8fdd053569a62d08ceff87cf1826
Somayeh <syoussefi600@gmail.com>
2022-01-30 12:44:17 -0500

    Create washington.m

---
+++ washington.m
@@ -0,0 +1,54 @@
+%function [ NPV, ROI ] = cashflow( power,costperwatt,DR, ER,SREC,ProducedElectricity,LifeTime)
+power = 6; %kw
+costperwatt = 5.88;
+ElectricityPrice = .1401;  %%% $/kWh
+ER =0.002807;
+DR=.03;
+ProducedElectricity = 9263.7; %monthly need*12 kwh
+loss = .005 ;
+SREC=480;
+LifeTime = 25;
+
+TaxRate = 0.06;
+InsuranceRate = 0.005;
+MaintananceRate = .005; 
+InitialCost = power * 1000 * costperwatt;
+InitialInvestment = InitialCost * (1+ TaxRate);
+
+%AnnCons = 12 * Load;
+
+%if (AnnCons > ProducedElectricity)
+%end
+
+NPV = ones(25,1);
+elecricity = ones(25,1);
+srec = ones(25,1);
+TaxCredit = .3 * InitialCost;
+max_tax_credit = power *1000 * 1;
+if TaxCredit > max_tax_credit;
+    TaxCredit = max_tax_credit;
+end
+NPV(1,1) = TaxCredit/(1 + DR) - InitialInvestment + SREC * ProducedElectricity/1000 + ElectricityPrice  * ProducedElectricity;
+indicator = 0;
+for year = 2:LifeTime
+    CashFlow = SREC * ProducedElectricity*(1-loss)^(year-1)/1000 + ElectricityPrice * (1 + ER)^(year - 1) * ProducedElectricity *(1-loss)^(year-1)-  (InsuranceRate + MaintananceRate) * InitialCost;
+    NPV(year) = NPV(year-1) + CashFlow/( 1 + DR )^( year - 1);
+    if (NPV(year) >= 0 && indicator == 0)
+        indicator = 1;
+        ROI = year;
+    end 
+    if indicator == 0
+        ROI = 0;
+    end
+
+end
+NPV
+ROI
+InitialInvestment
+%electricity
+
+
+
+
+
+%end
